eb8f2b9d419fe90e7f1ed1eb6b52a3dabe947a50
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 20:48:45 -0300

    musica

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿//MUDAR A COR de fundo
+//MAGENTA
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("puro e simples");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.WriteLine(@"Olha, Deus, o desenho que eu fiz pra ti
+Esse aqui é o meu pai
+Essa aqui é a mamãe e o meu irmão
+E esse aqui é você, e esse aqui é você
+E esse aqui é você
+Sabe, Deus, a minha mãe me falou mais cedo
+Que qualquer coisa que me der medo
+Posso correr pra você, posso correr pra você
+Posso correr pra você, posso correr pra você");
+
+Console.ResetColor();
+
+Console.WriteLine();
+
+//MUDAR A COR de fundo
+//azul
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine(@"E quando chegar lá
+Será que você vai me encontrar
+Se eu ficar no meu lugar secreto
+Discreto, bem quieto, esperando você me achar?");
+Console.ResetColor();
+
+
+ Console.WriteLine(@"quando chegar lá
+Será que você vai me encontrar
+Se eu ficar no meu lugar secreto
+Discreto, bem quieto, esperando você me achar?
+E quando chegar lá
+Será que você vai me encontrar
+Se eu ficar no meu lugar secreto
+Discreto, bem quieto, esperando você me achar?");
+
